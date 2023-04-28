791a2ca2558186d65e50f6d554adc00fdd1ca700
Mohamed <ahnbi32@gmail.com>
2023-04-28 19:14:47 +0200

    fixing some issus

--- Authentfication_Front/Angular-1/my-first-project/src/app/app.component.ts
+++ Authentfication_Front/Angular-1/my-first-project/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-project';
+
+  // vider le local
+  @HostListener('window:beforeunload', ['$event'])
+  clearLocalStorage(event: BeforeUnloadEvent) {
+    localStorage.clear();
+  }
 }
